c46a8a5ed76ad59f1ad56235d03474692cbaf705
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2023-12-19 21:49:19 +0100

    Addd limation on map movments

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,7 @@
 const gameEl = document.getElementById('game')
 const ships = {}
+const amount = 100
+
 const genarateMap = (rows, cols) => {
 	const map = document.createElement('div')
 	map.classList.add('map')
@@ -22,29 +24,62 @@ const genarateMap = (rows, cols) => {
 }
 genarateMap(41, 41)
 
+const getMapRelativeGaps = () => {
+	const map = document.querySelector('.map')
+	const offsets = map.getBoundingClientRect()
+
+	const windowHeight = window.innerHeight
+	const windowWidth = window.innerWidth
+	return {
+		top: offsets.top,
+		left: offsets.left,
+		bottom: windowHeight - offsets.bottom,
+		right: windowWidth - offsets.right,
+	}
+}
+
 const muoveMap = direction => {
 	console.log(direction)
+
+	const gaps = getMapRelativeGaps()
+	// if (
+	// 	gaps.top + amount > 100 ||
+	// 	gaps.bottom + amount > 100 ||
+	// 	gaps.left + amount > 100 ||
+	// 	gaps.right + amount > 100
+	// ) {
+	// 	console.log('Non è possibile muovere la mappa')
+	// 	return
+	// }
 	const map = document.querySelector('.map')
 	const transform = window.getComputedStyle(map).transform
 	// Estrarre i valori di traslazione (x, y)
 	const matrix = new DOMMatrixReadOnly(transform)
 	let x = matrix.m41
 	let y = matrix.m42
-	const amount = 100
 
 	// Aggiornare x o y in base alla direzione
 	switch (direction) {
 		case 'up':
-			y -= amount
+			if (gaps.bottom + amount < 100) {
+				y -= amount
+			}
 			break
 		case 'down':
-			y += amount
+			if (gaps.top + amount < 100) {
+				y += amount
+			}
 			break
 		case 'left':
-			x -= amount
+			if (gaps.right + amount < 100) {
+				x -= amount
+			}
 			break
 		case 'right':
-			x += amount
+			if (gaps.left + amount < 100) {
+				x += amount
+			}
+
 			break
 		default:
 			console.log('Direzione non valida')
@@ -52,10 +87,7 @@ const muoveMap = direction => {
 
 	// Applicare il nuovo valore di traslazione
 	map.style.transform = `translate(${x}px, ${y}px)`
-	const offsets = map.getBoundingClientRect()
-	var top = offsets.top
-	var left = offsets.left
-	console.log(top, left)
+	setTimeout(getMapRelativeGaps, 200)
 }
 
 document.addEventListener('keydown', e => {
