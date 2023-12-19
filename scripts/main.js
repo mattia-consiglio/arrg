2558551bd1d2e030163ab8628ef4dadc75e63597
Mattia Consiglio <mattiaconsiglio96@gmail.com>
2023-12-19 21:12:42 +0100

    Added map mouvement with keyboard arrows an button

--- scripts/main.js
+++ scripts/main.js
@@ -25,12 +25,40 @@ genarateMap(41, 41)
 const muoveMap = direction => {
 	console.log(direction)
 	const map = document.querySelector('.map')
-	const mapComuptedStyle = window.getComputedStyle(map)
-	const mapWidth = mapComuptedStyle.getPropertyValue('transform')
-	console.log(mapWidth)
+	const transform = window.getComputedStyle(map).transform
+	// Estrarre i valori di traslazione (x, y)
+	const matrix = new DOMMatrixReadOnly(transform)
+	let x = matrix.m41
+	let y = matrix.m42
+	const amount = 100
+
+	// Aggiornare x o y in base alla direzione
+	switch (direction) {
+		case 'up':
+			y -= amount
+			break
+		case 'down':
+			y += amount
+			break
+		case 'left':
+			x -= amount
+			break
+		case 'right':
+			x += amount
+			break
+		default:
+			console.log('Direzione non valida')
+	}
+
+	// Applicare il nuovo valore di traslazione
+	map.style.transform = `translate(${x}px, ${y}px)`
+	const offsets = map.getBoundingClientRect()
+	var top = offsets.top
+	var left = offsets.left
+	console.log(top, left)
 }
 
-document.addEventListener('keyup', e => {
+document.addEventListener('keydown', e => {
 	if (e.key === 'ArrowUp') {
 		muoveMap('up')
 	}
