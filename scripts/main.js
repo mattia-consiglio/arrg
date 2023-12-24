7216b5d31dd9b72547f3457757ed093397bc6910
Vikappa <vincenzo.e.costantini@gmail.com>
2023-12-24 16:20:53 +0100

    update animazione spostamento

--- scripts/main.js
+++ scripts/main.js
@@ -124,8 +124,7 @@ const popUpBaloon = function (cell) {
 	cella = cell
 	let i = cella.getAttribute(`data-row`)
 	let j = cella.getAttribute(`data-col`)
-	console.log(i)
-	console.log(j)
+
 	const removeBalloon = function () {
 		balloon.remove()
 	}
@@ -134,10 +133,23 @@ const popUpBaloon = function (cell) {
 		return parseInt(Math.sqrt(Math.pow(x - playerX, 2) + Math.pow(y - playerY, 2)))
 	}
 
-	const animaSpostaShipPlayer = function (x, y) {
-		let durataAnimazione = 0.5 * calcolaDistanza(x, y)
-	}
+	const animaSpostaShipPlayer = function (cellaDestinazione) {
+		const playerGraphic = document.getElementById('playerGraphic')
+		let currentPlayerX = playerShip.parentElement.offsetTop
+		let currentPlayerY = playerShip.parentElement.offsetLeft
+		let xDestinazione = cellaDestinazione.offsetTop
+		let yDestinazione = cellaDestinazione.offsetLeft
+		console.log(xDestinazione - currentPlayerX)
+		console.log(yDestinazione - currentPlayerY)
+		let durataAnimazione = 0.5 * calcolaDistanza(xDestinazione, yDestinazione)
 
+		playerGraphic.animate([
+			{ transform: `translate(${xDestinazione - currentPlayerX}px, ${yDestinazione - currentPlayerY}px)` }
+		], {
+			duration: durataAnimazione,
+			fill: 'forwards'
+		})
+	}
 
 	const movimentoPossibile = function (x, y) {
 		if (playerShipSpeed >= calcolaDistanza(x, y)) {
@@ -150,11 +162,11 @@ const popUpBaloon = function (cell) {
 	const spostaPlayer = function (cella, x, y) {
 		balloon.remove()
 		if (movimentoPossibile(x, y)) {
-			//animaSpostaShipPlayer(x, y)
-			setPlayerPosition(cella)
+			animaSpostaShipPlayer(cella)
 
 			setTimeout(function () {
-			}, 0.5 * calcolaDistanza(x, y))
+				setPlayerPosition(cella)
+			}, 0.5 * calcolaDistanza(x, y) * 1050)
 		}
 	}
 	if (document.getElementById('mouse-balloon')) {
@@ -360,7 +372,6 @@ const setPlayerPosition = function (cell) {
 
 const getCellaInizialeSpawn = function (xIniziale, yIniziale) {
 	let cella = document.querySelector(`div [data-row="${xIniziale}"][data-col="${yIniziale}"]`)
-	console.log(cella.getAttribute("data-row"))
 	return cella
 }
 
